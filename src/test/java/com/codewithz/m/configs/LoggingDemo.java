501f5f7fe6b286d9b52649c3f51278b118df3ec6
Zartab Nakhwa <zartab@codewithz.com>
2024-12-24 12:25:23 +0300

    Logs Demo

---
+++ src/test/java/com/codewithz/m/configs/LoggingDemo.java
@@ -0,0 +1,30 @@
+package com.codewithz.m.configs;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import org.testng.annotations.Test;
+
+public class LoggingDemo {
+
+    static final String BASE_URL="https://jsonplaceholder.typicode.com/posts";
+
+
+    @Test
+    void logTest(){
+        RestAssured
+                .given()
+                .log().all()
+//                OR
+                .log().headers()
+                .log().body()
+
+                .when()
+                .get(BASE_URL)
+                .then()
+                    //no conditions
+                    .log().headers()
+                    //with condition
+                    .log().ifValidationFails(LogDetail.ALL);
+
+    }
+}
